b02e4a79ea3c429d8ab43d2b938320da15108ee6
ice-mj <icevmj@gmail.com>
2018-09-21 15:27:55 +0800

    camera

---
+++ SourceCode/07.Camera/camera_class/frag.glsl
@@ -0,0 +1,14 @@
+#version 330 core
+
+in vec2 TexCoord;
+
+out vec4 FragColor;
+
+uniform sampler2D texture0;
+uniform sampler2D texture1;
+uniform float mixValue;
+
+void main()
+{
+	FragColor = mix(texture(texture0, TexCoord), texture(texture1, TexCoord), mixValue);
+}
